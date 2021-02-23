b748fded3dc1b63e20260cd5e8231b4c5acc60ee
molonepa <paolo.o.moloney@gmail.com>
2021-02-23 11:42:32 +0000

    implement basic player movement

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float lookSensitivity = 100.0f;
+
+    public Transform playerTransform;
+
+    float pitchAngle = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float mouseX =  Input.GetAxis("Mouse X") * lookSensitivity * Time.deltaTime;
+       float mouseY =  Input.GetAxis("Mouse Y") * lookSensitivity * Time.deltaTime;
+
+       pitchAngle -= mouseY;
+       pitchAngle = Mathf.Clamp(pitchAngle, -90.0f, 90.0f);
+
+       transform.localRotation = Quaternion.Euler(pitchAngle, 0.0f, 0.0f);
+       playerTransform.Rotate(Vector3.up * mouseX);
+    }
+}
