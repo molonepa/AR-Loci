d5b1d11495478c6e814f1013253fc272116b34e4
molonepa <paolo.o.moloney@gmail.com>
2021-02-26 08:52:41 +0000

    add default object instantiation via ui button

---
+++ Assets/Scripts/CreateMemoryNote.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMemoryNote : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public void InstantiateMemoryNote() {
+        Instantiate(prefab, new Vector3(0, 1, 0), Quaternion.identity);
+    }
+}
