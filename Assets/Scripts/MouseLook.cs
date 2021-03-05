2ad79b78b6edf52315f7d8001e097b8041e96e56
molonepa <paolo.o.moloney@gmail.com>
2021-03-05 09:07:57 +0000

    implement object instantiation at player cursor

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float lookSensitivity = 300.0f;
-
     public Transform playerTransform;
+    public GameObject prefab;
 
     float pitchAngle = 0.0f;
+    RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        Look();
+        CastRay();
+    }
+
+    void Look() {
        float mouseX =  Input.GetAxis("Mouse X") * lookSensitivity * Time.deltaTime;
        float mouseY =  Input.GetAxis("Mouse Y") * lookSensitivity * Time.deltaTime;
 
@@ -27,5 +32,24 @@ public class MouseLook : MonoBehaviour
 
        transform.localRotation = Quaternion.Euler(pitchAngle, 0.0f, 0.0f);
        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+
+    void CastRay() {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100.0f)) {
+            Debug.DrawRay(transform.position, transform.forward, Color.green);
+        }
+        if (Input.GetButtonDown("Fire1")) {
+            CreateObjectAtRay();
+        }
+    }
+
+    void CreateObjectAtRay() {
+        if (hit.collider.name=="Main Walls") {
+            Instantiate(prefab, hit.point, Quaternion.identity);
+            Debug.Log("Hit");
+        }
+        else {
+            Debug.Log("Miss");
+        }
     }
 }
